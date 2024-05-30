96f111e184bf8eb3141ab87b07762d68b865beb3
gt-min <gt_min@naver.com>
2024-05-30 10:10:24 +0900

    20240530

--- DAY04_01_DQL.sql
+++ DAY04_01_DQL.sql
@@ -40,3 +40,51 @@ SELECT subj_no
   FROM enroll_t
  GROUP BY subj_no
 HAVING COUNT(*) = 0;
+
+/*
+  ORDER BY 절
+  1. 조회 결과를 정렬할 때 사용한다.
+  2. 정렬 방식
+    1) 오름차순 : ASC 또는 생략
+    2) 내림차순 : DESC
+*/
+
+-- 1. 이름의 내림차순으로 조회하기
+SELECT stu_no, stu_name
+  FROM student_t
+ ORDER BY stu_name DESC;
+
+-- 2. 과목명의 오름차순으로 조회하기
+SELECT subj_no, subj_name
+  FROM subject_t
+ ORDER BY subj_name;
+
+/*
+  SELECT 문의 실행 순서
+  
+  SELECT     -- 5
+    FROM     -- 1
+   WHERE     -- 2
+   GROUP BY  -- 3
+  HAVING     -- 4
+   ORDER BY  -- 6
+*/
+-- 1. 학생별로 수강신청을 몇 건 했는지 조회하기
+SELECT stu_no   AS 학번
+     , COUNT(*) AS 건수
+  FROM enroll_t
+ WHERE 학번 IS NOT NULL
+ GROUP BY 학번;
+
+-- 2. 한 과목만 수강 신청한 학생 조회하기
+SELECT stu_no   AS 학번
+     , COUNT(*) AS 건수
+  FROM enroll_t
+ GROUP BY 학번
+HAVING 건수 = 1;
+
+-- 3. 과목명의 오름차순으로 조회하기
+SELECT subj_no   AS 과목번호
+     , subj_name AS 과목명
+  FROM subject_t
+ ORDER BY 과목명;
